ab849b34ff065f20cb2b77d90e0badf7c4ee2046
Дмитро Кобилінський <dmytro.kobylinskyi.pz.2020@lpnu.ua>
2022-10-27 20:25:21 +0300

    takeBreakCommit2

--- src/main/java/pizzeria/entity/Pizzeria.java
+++ src/main/java/pizzeria/entity/Pizzeria.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import pizzeria.entity.cooks.*;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/pizzeria/entity/cooks/Cook.java
+++ src/main/java/pizzeria/entity/cooks/Cook.java
@@ -3,14 +3,28 @@ package pizzeria.entity.cooks;
 import pizzeria.entity.*;
 
 import java.util.List;
+import java.util.Random;
 
 public abstract class Cook {
 
     protected static Pizzeria pizzeria = new Pizzeria();
-    List<Pizza> pizzas; //pizzas - піци в одному замовленні
+    List<Pizza> pizzas; // pizzas - піци в одному замовленні
     protected CookState state;
 
-    public abstract void takeBreak();
+    public void takeBreak() {
+        boolean haveBreak = new Random().nextInt(1, 10) == 5 ? true : false;
+
+        if (haveBreak) {
+            try {
+                state = CookState.OUT;
+                Thread.sleep(1000); // 1000 это 1 секунда
+                state = CookState.AVAILABLE;
+
+            } catch (Exception ex) {
+            }
+
+        }
+    }
 
     public abstract void takeTask();
 
