e1ff7fa811a547affb49747d79831a0ac55ebf0e
olharubel <rubel.olha@gmail.com>
2022-10-27 20:45:09 +0300

    Singleton for Pizzeria Choose cashier for Customer

--- src/main/java/pizzeria/entity/Customer.java
+++ src/main/java/pizzeria/entity/Customer.java
@@ -3,8 +3,8 @@ package pizzeria.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 
 @AllArgsConstructor
 @Getter
@@ -13,14 +13,7 @@ public class Customer {
     private Order order;
 
     public void chooseCashier(List<Cashier> allCashiers) {
-        int minValue = allCashiers.stream().mapToInt(Cashier::getCustomersAmount).min().orElse(-1);
-
-        List<Cashier> minCustomersCashiers = allCashiers.stream().filter(s -> s.getCustomersAmount() == minValue)
-                .toList();
-
-        Cashier selectedCashier = minCustomersCashiers.get(new Random().nextInt(minCustomersCashiers.size()));
-
-        selectedCashier.addCustomer(this);
-
+        var cashier = allCashiers.stream().min(Comparator.comparing(Cashier::getCustomersAmount)).orElseThrow();
+        cashier.addCustomer(this);
     }
 }
