3865d4b2fd165165ea56d6fb8b6378855f6ee5d8
Oleh Hembarovskyi <oleh.hembarovskyi.pz.2020@lpnu.ua>
2022-11-16 20:03:54 +0200

    fix: tables

--- Pizzeria_UI/src/main/java/pizzeria_ui/TableController.java
+++ Pizzeria_UI/src/main/java/pizzeria_ui/TableController.java
@@ -6,52 +6,48 @@ import javafx.scene.layout.GridPane;
 import pizzeria.entity.Pizzeria;
 import pizzeria.entity.Table;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TableController {
     private final GridPane tablesGrid;
     private final List<Map<ImageView, Table>> imageViews = new ArrayList<>();
-    private Image emptyTable;
-    private Image eatingTable;
-    private Image waitingTable;
+    private final Image emptyTable;
+    private final Image eatingTable;
+    private final Image waitingTable;
+    private final Image noneTable;
+    private final int tileSize;
+    private final int columnCount;
+    private final int rowCount;
 
     public TableController(GridPane tablesGrid) {
         this.tablesGrid = tablesGrid;
-    }
 
-    public void init() {
         emptyTable = new Image("table_empty.jpg");
         eatingTable = new Image("table_eating.jpg");
         waitingTable = new Image("table_waiting.jpg");
+        noneTable = new Image("none_table.png");
+
         int sizeWidth = (int) tablesGrid.getPrefWidth();
         int sizeHeight = (int) tablesGrid.getPrefHeight();
         int tileWidth = sizeWidth / tablesGrid.getColumnCount();
         int tileHeight = sizeHeight / tablesGrid.getRowCount();
-        int tileSize = Math.min(tileWidth, tileHeight);
+        tileSize = Math.min(tileWidth, tileHeight);
 
-        int columnCount = tablesGrid.getColumnCount();
-        int rowCount = tablesGrid.getRowCount();
+        columnCount = tablesGrid.getColumnCount();
+        rowCount = tablesGrid.getRowCount();
 
-        int tablesCounter = 0;
-        int tablesAmount = Pizzeria.getInstance().getTables().size();
         for (int i = 0; i < columnCount; i++) {
             Map<ImageView, Table> column = new HashMap<>();
 
             for (int j = 0; j < rowCount; j++) {
-                if (tablesAmount <= tablesCounter) {
-                    break;
-                }
-
                 ImageView imageView = new ImageView();
+                imageView.setImage(noneTable);
                 imageView.setFitHeight(tileSize);
                 imageView.setFitWidth(tileSize);
                 tablesGrid.add(imageView, i, j);
-                column.put(imageView, Pizzeria.getInstance().getTables().get(tablesCounter));
-                tablesCounter++;
+                column.put(imageView, null);
             }
+
             imageViews.add(column);
             tablesGrid.getColumnConstraints().get(i).setPrefWidth(tileSize);
         }
@@ -61,13 +57,39 @@ public class TableController {
         }
     }
 
+    public void init() {
+        int tablesCounter = 0;
+        int tablesAmount = Pizzeria.getInstance().getTables().size();
+
+        for (int i = 0; i < columnCount; i++) {
+            Map<ImageView, Table> column = imageViews.get(i);
+            Iterator<ImageView> imageViewIterator = column.keySet().iterator();
+
+            for (int j = 0; j < rowCount; j++) {
+                if (tablesAmount <= tablesCounter || !imageViewIterator.hasNext()) {
+                    break;
+                }
+
+                ImageView imageView = imageViewIterator.next();
+                imageView.setImage(noneTable);
+                imageView.setFitHeight(tileSize);
+                imageView.setFitWidth(tileSize);
+                column.put(imageView, Pizzeria.getInstance().getTables().get(tablesCounter));
+                tablesCounter++;
+            }
+        }
+    }
+
     public void update() {
         for (Map<ImageView, Table> viewsTables : imageViews) {
-            for (var viewTable : viewsTables.entrySet()) {
+            for (var viewTable : viewsTables.entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() != null)
+                    .toList()) {
                 switch (viewTable.getValue().getTableState()) {
-                case EMPTY -> viewTable.getKey().setImage(emptyTable);
-                case CUSTOMER_EATING -> viewTable.getKey().setImage(eatingTable);
-                case CUSTOMER_WAITING -> viewTable.getKey().setImage(waitingTable);
+                    case EMPTY -> viewTable.getKey().setImage(emptyTable);
+                    case CUSTOMER_EATING -> viewTable.getKey().setImage(eatingTable);
+                    case CUSTOMER_WAITING -> viewTable.getKey().setImage(waitingTable);
                 }
             }
         }
